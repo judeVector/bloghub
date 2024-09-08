118093dd42fc34feb4d84b1e5e7c93e0a459701c
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-09 03:08:34 +0400

    Add Test for blog_api endpoints

--- src/blog_api/tests.py
+++ src/blog_api/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from blog.models import Post, Category
+
+User = get_user_model()
+
+
+class PostTests(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="django")
+        self.user = User.objects.create_user(
+            username="testuser1", password="password123"
+        )
+
+        user = User.objects.get(username="testuser1")
+        self.client.force_authenticate(user=user)
+
+    def test_view_post(self):
+        url = reverse("blog_api:listcreate")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_post(self):
+        url = reverse("blog_api:listcreate")
+        payload = {
+            "title": "Exploring Django Models",
+            "author": 1,
+            "excerpt": "This is a second test to see whether it will slug",
+            "content": "This content is suppose to be sluggified in 2 number  ",
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- src/blog_api/urls.py
+++ src/blog_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = "blog_api"
+
 urlpatterns = [
     path("", PostListCreate.as_view(), name="listcreate"),
     path("<int:pk>", PostDetailDelete.as_view(), name="detailcreate"),
