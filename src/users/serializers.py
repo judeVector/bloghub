496c75066c473109b4d3694e588ba1b8bb913f2b
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-13 17:44:02 +0400

    Update the serializer to use get_user_model()

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
-from .models import NewUser
+NewUser = get_user_model()
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
